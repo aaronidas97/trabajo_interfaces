571611c035370c4c6cca59db4a2c21fd6a9724f1
aaronidas97 <freiresousa97@gmail.com>
2024-01-22 23:40:07 +0100

    markdown2

--- README.md
+++ README.md
@@ -1,28 +1,51 @@
 # Repositorio: Proyecto Pokémon 1ra Generación
 
-## Archivos HTML
+## Descripción
 
-- **index.html**: Página principal del proyecto.
-- **pokemon.html**: Página específica para detalles de Pokémon.
+Este repositorio alberga un pequeño proyecto de página web dedicado a los Pokémon de la primera generación. La página principal, `index.html`, actúa como el punto de entrada, proporcionando una visión general y acceso a detalles específicos de cada Pokémon a través de la página `pokemon.html`.
 
-## Estilos CSS
+## Contenido del Proyecto
 
-- **card-styles.css**: Estilos para tarjetas de Pokémon.
-- **lista-styles.css**: Estilos para listas en la página.
-- **styles.css**: Estilos generales del proyecto.
+### Archivos HTML
 
-## Scripts JavaScript
+- **index.html**: La página principal que da la bienvenida a los visitantes y proporciona una vista general del proyecto.
+- **pokemon.html**: Página detallada con información específica de cada Pokémon.
 
-- **fetch.js**: Manejo de solicitudes y respuestas (fetch).
+### Estilos CSS
+
+- **card-styles.css**: Contiene estilos para las tarjetas de Pokémon en la página principal.
+- **lista-styles.css**: Estilos adicionales para listas utilizadas en diferentes secciones del proyecto.
+- **styles.css**: Hoja de estilos principal que abarca el diseño general del proyecto.
+
+### Scripts JavaScript
+
+- **fetch.js**: Script que maneja las solicitudes y respuestas (fetch) para la obtención de datos.
 - **index.js**: Lógica específica para la página principal.
-- **pokemon-js**: Scripts relacionados con la página de detalles.
-- **pokemonClass.js**: Clase para representar a un Pokémon.
-- **pokemonList.js**: Lógica para gestionar la lista de Pokémon.
+- **pokemon-js**: Contiene scripts relacionados con la página de detalles de Pokémon.
+- **pokemonClass.js**: Define una clase para representar a un Pokémon y gestionar sus atributos.
+- **pokemonList.js**: Contiene la lógica para gestionar la lista de Pokémon y su presentación en la página.
 
-## Imágenes
+### Imágenes
 
-- **fondo.webp**: Imagen de fondo para el proyecto.
-- **logo.png**: Logotipo del proyecto.
-- **logoLetras.webp**: Logotipo con letras.
-- **pokemon-party.jpg**: Imagen temática de Pokémon.
+- **fondo.webp**: Imagen de fondo que crea una atmósfera temática para el proyecto.
+- **logo.png**: Logotipo del proyecto que sirve como identificador visual.
+- **logoLetras.webp**: Variante del logotipo con elementos de letras adicionales.
+- **pokemon-party.jpg**: Imagen temática que puede utilizarse para ilustrar
 
+## La decisión de mostrar a los Pokémon en una lista uniforme, de forma exactamente igual y con los mismos tamaños, se relaciona directamente con varias leyes fundamentales de la Experiencia de Usuario (UX). A continuación, se explica cómo esta elección está vinculada a las leyes mencionadas:
+
+### Ley de Proximidad
+
+La presentación uniforme de los Pokémon en la lista, con elementos cercanos unos a otros, sigue la Ley de Proximidad. Al organizar visualmente a los Pokémon en proximidad, se establece una relación inmediata entre ellos. Esto facilita la identificación de grupos de Pokémon y proporciona una estructura coherente que ayuda a los usuarios a comprender la disposición de la información.
+
+### Ley de Similitud
+
+La uniformidad en la presentación, con elementos visuales idénticos y tamaños uniformes, respeta la Ley de Similitud. Al compartir características visuales consistentes, los Pokémon se perciben como relacionados entre sí. Esto contribuye a una experiencia de usuario más clara y comprensible.
+
+### Ley de la Semejanza
+
+La presentación uniforme refuerza la Ley de la Semejanza al destacar las características comunes entre los Pokémon. Los elementos visuales idénticos refuerzan la percepción de que los Pokémon comparten similitudes y pertenecen a la misma categoría.
+
+### Ley de la Continuidad
+
+La disposición uniforme de los Pokémon en la lista sigue la Ley de la Continuidad al alinear visualmente los elementos. Esto ayuda a los usuarios a ver la relación entre los Pokémon y proporciona una experiencia fluida en la exploración de la lista.
--- scripts/pokemonList.js
+++ scripts/pokemonList.js
@@ -2,15 +2,27 @@ import { fetchListFromUrl, fetchDataFromUrl } from "./fetch.js";
 import { Pokemon } from "./pokemonClass.js";
 
 
-const url = "https://pokeapi.co/api/v2/pokemon/";
+let url = "https://pokeapi.co/api/v2/pokemon/";
 
 let listaPokemon = [];
+    let lista = document.getElementById("pokemon");
+
+    const nextPage = document.getElementById("nextLink");
 
 document.addEventListener("DOMContentLoaded", () => {
-    let lista = document.getElementById("pokemon");
 
     fetchListFromUrl(url)
         .then((listaJson) => {
+            console.log(listaJson.next)
+
+            nextPage.onclick = () =>{
+
+                
+                nextFetch(listaJson.next)
+
+
+
+            };
             let pokemonPromises = Object.values(listaJson.results).map((values) => {
 
                 
@@ -29,46 +41,78 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .then((pokemones) => {
             listaPokemon = pokemones;
-            console.log(listaPokemon);
 
-            listaPokemon.forEach((pokemon) => {
-                console.log(pokemon.nombre);
-                
-
-                let contenedor = document.createElement('div');
-                contenedor.className ="card card-body col";
-                contenedor.id = "contenedor";
-
-                let imgPokemon = document.createElement("img");
-                imgPokemon.className = " imagenPokemon";
-                imgPokemon.id = "imagen";
-                imgPokemon.alt = "Imagen de " + pokemon.nombre;
-                imgPokemon.src = pokemon.imagen;
-            
-                contenedor.appendChild(imgPokemon);
-                imgPokemon.onclick = () => {
-                    const url = "https://pokeapi.co/api/v2/pokemon/";
-
-                    fetchDataFromUrl(url + pokemon.nombre)
-                     .then((pokemon) => {
-                             
-                        window.location.href = `pokemon.html?name=${pokemon.nombre}&image=${pokemon.imagen}&weight=${pokemon.weight}`;
-    
-    
-                        })
-                        .catch((error) => {
-                         console.error("Error fetching data:", error);
-                         });
-    
-                }
-                lista.appendChild(contenedor);
-
-
-
-              
-
-
-            });
-        }).then
+        setPokemonInfo(listaPokemon);
+        })
 });
 
+function setPokemonInfo(listaPokemon) {
+    listaPokemon.forEach((pokemon) => {
+        
+
+        let contenedor = document.createElement('div');
+        contenedor.className ="card card-body col";
+        contenedor.id = "contenedor";
+
+        let imgPokemon = document.createElement("img");
+        imgPokemon.className = " imagenPokemon";
+        imgPokemon.id = "imagen";
+        imgPokemon.alt = "Imagen de " + pokemon.nombre;
+        imgPokemon.src = pokemon.imagen;
+    
+        contenedor.appendChild(imgPokemon);
+        imgPokemon.onclick = () => {
+            const url = "https://pokeapi.co/api/v2/pokemon/";
+
+            fetchDataFromUrl(url + pokemon.nombre)
+             .then((pokemon) => {
+                     
+                window.location.href = `pokemon.html?name=${pokemon.nombre}&image=${pokemon.imagen}&weight=${pokemon.weight}`;
+
+
+                })
+                .catch((error) => {
+                 console.error("Error fetching data:", error);
+                 });
+
+        }
+        lista.appendChild(contenedor);
+
+
+
+      
+
+
+    });
+}
+
+function nextFetch(nextPokemons){
+    
+    fetchListFromUrl(nextPokemons)
+        .then((listaJson) => {
+            
+
+            nextPage.onclick = () =>{
+
+                url = listaJson.next;
+
+
+
+            };
+            let pokemonPromises = Object.values(listaJson.results).map((values) => {
+
+                
+                let pokemon = new Pokemon();
+                pokemon.nombre = values.name;
+
+                return fetchListFromUrl(values.url)
+                    .then((datosPokemon) => {
+                        pokemon.imagen = datosPokemon.sprites.front_default;
+                        pokemon.weight = datosPokemon.weight;
+                        return pokemon;
+                    });
+            });
+
+            return Promise.all(pokemonPromises);
+        })
+}
