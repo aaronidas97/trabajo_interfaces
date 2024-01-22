8bba5c8e711e27b77be1a6b55d48bde675565a18
aaronidas97 <freiresousa97@gmail.com>
2024-01-23 00:51:58 +0100

    comentarios finales y trabajo terminado

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,19 @@
 import { fetchDataFromUrl } from "./fetch.js";
 
+// Obtener el formulario y agregar un evento de escucha para el envío
 const form = document.getElementById("searchForm");
-        
-form.addEventListener("submit",  (event) => {
-
+form.addEventListener("submit", (event) => {
     event.preventDefault(); // Evitar la recarga de la página por defecto
 
+    // Obtener el nombre buscado del campo de entrada y convertirlo a minúsculas
     const nombreBuscado = document.getElementById("pokemonName").value.toLowerCase();
     const url = "https://pokeapi.co/api/v2/pokemon/";
 
+    // Realizar una solicitud para obtener información del Pokémon buscado
     fetchDataFromUrl(url + nombreBuscado)
         .then((pokemon) => {
-          window.location.href = `pokemon.html?name=${pokemon.nombre}&image=${pokemon.imagen}&weight=${pokemon.weight}`;
-
-
+            // Redirigir a la página del Pokémon con parámetros en la URL
+            window.location.href = `pokemon.html?name=${pokemon.nombre}&image=${pokemon.imagen}&weight=${pokemon.weight}`;
         })
         .catch((error) => {
             console.error("Error fetching data:", error);
--- scripts/pokemonList.js
+++ scripts/pokemonList.js
@@ -1,27 +1,30 @@
 import { fetchListFromUrl, fetchDataFromUrl } from "./fetch.js";
 import { Pokemon } from "./pokemonClass.js";
 
+// URL base para obtener la lista de Pokémon
+const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-let url = "https://pokeapi.co/api/v2/pokemon/";
-
+// Array para almacenar la lista de Pokémon
 let listaPokemon = [];
-    let lista = document.getElementById("pokemon");
 
-    const nextPage = document.getElementById("nextLink");
-    let nuevaUrl="";
+// Elemento de la lista en el HTML
+const lista = document.getElementById("pokemon");
+
 document.addEventListener("DOMContentLoaded", () => {
 
-    fetchListFromUrl(url)
+    // Obtener la lista de Pokémon usando la función fetchListFromUrl
+    fetchListFromUrl(baseUrl)
         .then((listaJson) => {
-        
-            nuevaUrl = listaJson.next;
-           
-            let pokemonPromises = Object.values(listaJson.results).map((values) => {
+            // Guardar la próxima URL para obtener más Pokémon
+            const nextUrl = listaJson.next;
 
-                
+            // Crear promesas para obtener información detallada de cada Pokémon
+            let pokemonPromises = Object.values(listaJson.results).map((values) => {
+                // Crear una instancia de la clase Pokemon para cada Pokémon
                 let pokemon = new Pokemon();
                 pokemon.nombre = values.name;
 
+                // Obtener información detallada del Pokémon
                 return fetchListFromUrl(values.url)
                     .then((datosPokemon) => {
                         pokemon.imagen = datosPokemon.sprites.front_default;
@@ -30,52 +33,52 @@ document.addEventListener("DOMContentLoaded", () => {
                     });
             });
 
+            // Esperar a que todas las promesas se resuelvan
             return Promise.all(pokemonPromises);
         })
         .then((pokemones) => {
+            // Almacenar la lista completa de Pokémon
             listaPokemon = pokemones;
 
-        setPokemonInfo(listaPokemon);
-        })
+            // Llamar a la función para mostrar la información de los Pokémon
+            setPokemonInfo(listaPokemon);
+        });
 });
 
+// Función para mostrar la información de los Pokémon en la página
 function setPokemonInfo(listaPokemon) {
     listaPokemon.forEach((pokemon) => {
-        
-
+        // Crear un contenedor para cada Pokémon
         let contenedor = document.createElement('div');
-        contenedor.className ="card card-body col";
+        contenedor.className = "card card-body col";
         contenedor.id = "contenedor";
 
+        // Crear una imagen para el Pokémon
         let imgPokemon = document.createElement("img");
-        imgPokemon.className = " imagenPokemon";
+        imgPokemon.className = "imagenPokemon";
         imgPokemon.id = "imagen";
         imgPokemon.alt = "Imagen de " + pokemon.nombre;
         imgPokemon.src = pokemon.imagen;
-    
+
+        // Agregar la imagen al contenedor
         contenedor.appendChild(imgPokemon);
+
+        // Agregar un evento click para redirigir a la página del Pokémon
         imgPokemon.onclick = () => {
-            const url = "https://pokeapi.co/api/v2/pokemon/";
+            const pokemonDetailsUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-            fetchDataFromUrl(url + pokemon.nombre)
-             .then((pokemon) => {
-                     
-                window.location.href = `pokemon.html?name=${pokemon.nombre}&image=${pokemon.imagen}&weight=${pokemon.weight}`;
-
-
+            // Obtener información adicional del Pokémon antes de redirigir
+            fetchDataFromUrl(pokemonDetailsUrl + pokemon.nombre)
+                .then((pokemon) => {
+                    // Redirigir a la página del Pokémon con parámetros en la URL
+                    window.location.href = `pokemon.html?name=${pokemon.nombre}&image=${pokemon.imagen}&weight=${pokemon.weight}`;
                 })
                 .catch((error) => {
-                 console.error("Error fetching data:", error);
-                 });
+                    console.error("Error fetching data:", error);
+                });
+        };
 
-        }
+        // Agregar el contenedor a la lista en el HTML
         lista.appendChild(contenedor);
-
-
-
-      
-
-
     });
 }
-
